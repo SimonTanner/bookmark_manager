5f47ed6a02475d53cee7ff91174075a864b078a1
SimonTanner <simonthetanner@hotmail.com>
2017-05-07 17:51:21 +0100

    Rakefile added for auto_upgrade or auto_migrate of database

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -3,9 +3,19 @@ feature 'Adding tags to urls' do
     visit 'links/new'
     fill_in 'url', with: 'http://www.hotmail.com'
     fill_in 'title', with: 'My emails'
-    fill_in 'tags', with: 'Emails'
+    fill_in 'tags', with: 'emails'
     click_button 'Create'
     link = Link.first
-    expect(link.tags.map(&:name)).to include('Emails')
+    expect(link.tags.map(&:name)).to include('emails')
+  end
+
+  scenario 'Add a tag to a bookmarked url' do
+    visit 'links/new'
+    fill_in 'url', with: 'http://www.makersacademy.com'
+    fill_in 'title', with: 'Coding School'
+    fill_in 'tags', with: 'education ruby'
+    click_button 'Create'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
   end
 end
